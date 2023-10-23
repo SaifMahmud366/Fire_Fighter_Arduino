043a8f77989e62ec153f07874449391bf9e64002
Saif Mahmud <saifmahmud366@gmail.com>
2023-10-23 23:07:24 +0600

    Flame Sensor Setup
    flame sensor and RED,GREEN led setup

---
+++ Flame_Sensor/Flame_Sensor_and_LED.ino
@@ -0,0 +1,49 @@
+#define Fire_sensor 8
+#define Buzzer 9
+#define RedLED 10
+#define GreenLED 11 // Define the pin for the green LED
+#define Relay_Pin 7
+
+
+#define ldelay 500
+#define bdelay 500
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(Fire_sensor, INPUT);
+    pinMode(Buzzer, OUTPUT);
+    pinMode(RedLED, OUTPUT);
+    pinMode(GreenLED, OUTPUT); // Set GreenLED pin as OUTPUT
+}
+
+void loop() {
+    if (digitalRead(Fire_sensor) == LOW) {
+        alert();
+    } else {
+        digitalWrite(Buzzer, LOW);
+        digitalWrite(RedLED, LOW);
+        digitalWrite(GreenLED, HIGH); // Turn on the green LED when no flame is detected
+    }
+}
+
+void alert() {
+    digitalWrite(RedLED, HIGH);
+    digitalWrite(Buzzer, HIGH);
+    digitalWrite(GreenLED, LOW); // Turn off the green LED during alert
+    delay(ldelay);
+    digitalWrite(RedLED, LOW);
+    digitalWrite(Buzzer, LOW);
+    delay(ldelay);
+    digitalWrite(RedLED, HIGH);
+    digitalWrite(Buzzer, HIGH);
+    delay(ldelay);
+    digitalWrite(RedLED, LOW);
+    digitalWrite(Buzzer, LOW);
+    delay(ldelay);
+    digitalWrite(RedLED, HIGH);
+    digitalWrite(Buzzer, HIGH);
+    delay(ldelay);
+    digitalWrite(RedLED, LOW);
+    digitalWrite(Buzzer, LOW);
+    delay(ldelay);
+}
